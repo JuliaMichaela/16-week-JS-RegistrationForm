7269aff1d36755460889185f76ed512c3ea29287
JuliaMichaela <juliaperez@yandex.ru>
2024-12-02 23:47:05 +0300

    added const elements forms

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,6 @@
 const formRegister = document.forms.register;
+const nameInput = formRegister.elements.name;
+const emailInput = formRegister.elements.email;
+const ageInput = formRegister.elements.age;
+const genderInput = formRegister.elements.gender;
+const passwordInput = formRegister.elements.password;
